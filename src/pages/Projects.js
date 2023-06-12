ffdc6a2820fa57262f7e96e7297e46472418e5ab
Tatiana Obando <PATH="/opt/homebrew/bin:$PATH>
2023-06-11 19:20:12 -0700

    Add assets

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import ProjectItem from '../components/ProjectItem';
+import Proj1 from '../assets/running.jpeg';
+import Proj2 from '../assets/spotify.jpeg';
+import "../styles/Projects.css";
+import Proj3 from "../assets/glenn-carstens-peters-RLw-UC03Gwc-unsplash.jpg"
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className='projects'>
+        <h1>My Personal Projects</h1>
+    <div className='projectList'>
+        <ProjectItem name="Oh My Run" image={Proj1}/>
+        <ProjectItem name="Spotify Clone" image={Proj2}/>
+        <ProjectItem name="Task List" image={Proj3}/>
+    </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
